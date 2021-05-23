0256e6b26e317536fe01af09212d7242e95a84d7
RickForce <Rick@Rickforce.com>
2021-05-23 13:24:16 +0200

    fix for db env setting

--- config.py
+++ config.py
@@ -5,8 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
-    DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,
-                                                                                            'telegram-bot.db')
+
+    db_env_setting = os.environ.get('DATABASE_URL')
+    if db_env_setting is not None and db_env_setting.startswith('postgres'):
+        db_env_setting = db_env_setting.replace('postgres', 'postgresql')
+
+    DATABASE_URL = db_env_setting or 'sqlite:///' + os.path.join(basedir, 'telegram-bot.db')
 
 
 def ready_env():

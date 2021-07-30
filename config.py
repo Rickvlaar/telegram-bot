3f063c0fd9f5b2161221027c8e662c29cd13da2f
RickForce <Rick@Rickforce.com>
2021-07-30 18:46:41 +0200

    automatic kratjes warning

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
     HHPC_TOKEN = os.environ.get('HHPC_TOKEN')
-    MEMBERS = os.environ.get('MEMBERS')
+    HHPC_CHAT_ID = os.environ.get('HHPC_TOKEN')
+    MEMBERS = os.environ.get('HHPC_CHAT_ID')
 
     db_env_setting = os.environ.get('DATABASE_URL')
     if db_env_setting is not None and db_env_setting.startswith('postgres'):
--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -1,6 +1,7 @@
 from telegram import Bot
 from data_model import Kratjes, db_session
 from datetime import datetime
+from config import Config
 import asyncio
 
 
@@ -8,12 +9,14 @@ async def get_expired_bets(bot: Bot):
     while True:
         session = db_session()
         kratjes = session.query(Kratjes).order_by(Kratjes.created_date).all()
-        message_text = ''
+        message_text = 'De volgende kratjes zijn verschuldigd! \n\n'
         for bet in kratjes:
-            message_text += f'{bet.better} stelt: '
-            bet.due_date = bet.due_date.strftime('%d-%m-%Y') if bet.due_date else 'het einde van dit leven'
-            message_text += '"{1}" voor "{2}" met "{0}" als onderpand\n\n'.format(
-                    bet.stake, bet.bet_description, bet.due_date)
-        bot.send_message(chat_id='', text=message_text)
+            if bet.due_date < datetime.now():
+                message_text += f'{bet.better} stelt: '
+                bet.due_date = bet.due_date.strftime('%d-%m-%Y') if bet.due_date else 'het einde van dit leven'
+                message_text += '"{1}" voor "{2}" met "{0}" als onderpand\n\n'.format(
+                        bet.stake, bet.bet_description, bet.due_date)
+        bot.send_message(chat_id=Config.HHPC_CHAT_ID, text=message_text)
+
         # wait for 1 hours
-        await asyncio.sleep(12000)
+        await asyncio.sleep(60*60*24)

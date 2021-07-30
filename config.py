6cce2262f2f49d493aef329499364515f5765bd8
RickForce <Rick@Rickforce.com>
2021-07-30 19:01:33 +0200

    config fix

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
     HHPC_TOKEN = os.environ.get('HHPC_TOKEN')
-    HHPC_CHAT_ID = os.environ.get('HHPC_TOKEN')
-    MEMBERS = os.environ.get('HHPC_CHAT_ID')
+    HHPC_CHAT_ID = os.environ.get('HHPC_CHAT_ID')
+    MEMBERS = os.environ.get('MEMBERS')
 
     db_env_setting = os.environ.get('DATABASE_URL')
     if db_env_setting is not None and db_env_setting.startswith('postgres'):
